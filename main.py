482bc11e2c8cdc08f28aedbc9ba3fa48ebca2014
Ananya <afmtrendaid@gmail.com>
2023-12-05 19:56:15 +0300

    all

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import asyncio
 import logging
 import sys
-from os import getenv
-from typing import Any, Dict
+import sqlite3
 from aiogram import Bot, Dispatcher, F, Router, html
 from aiogram.enums import ParseMode
 from aiogram.filters import Command, CommandStart
@@ -19,10 +18,24 @@ from aiogram.types import (
 
 TOKEN = "6638058557:AAHVL5l3wwHryZ9mUtgK74VPRsfwVQySuBM"
 
+# SQLite database initialization
+conn = sqlite3.connect('user_data.db')
+cursor = conn.cursor()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        user_id INTEGER PRIMARY KEY,
+        full_name TEXT,
+        phone_number TEXT,
+        role TEXT
+    )
+''')
+
+conn.commit()
+
 form_router = Router()
 
-
 class UserRegistration(StatesGroup):
+    user_id = State()
     full_name = State()
     phone_number = State()
     role = State()
@@ -55,6 +68,8 @@ async def process_full_name(message: Message, state: FSMContext):
 @form_router.message(UserRegistration.phone_number)
 async def process_phone_number(message: Contact, state: FSMContext):
     print(message.contact)
+    user_id = message.contact.user_id  # Use Telegram user ID as primary key
+    await state.update_data(user_id=user_id)
     phone_number = message.contact.phone_number
     await state.update_data(phone_number=phone_number)
     await message.answer(f"Great! I got your phone number: {phone_number}. Now, choose your role.",
@@ -65,19 +80,20 @@ async def process_phone_number(message: Contact, state: FSMContext):
                              resize_keyboard=True
                          ))
 
-    print(state.get_data())
     await state.set_state(UserRegistration.role)
-
-
 
 @form_router.message(UserRegistration.role)
 async def process_role(message: Message, state: FSMContext):
     role = message.text
     await state.update_data(role=role)
     
+    # Retrieve user data from state
     data = await state.get_data()
 
-    print(data)
+    # Store the user details in SQLite database
+    cursor.execute("INSERT OR REPLACE INTO users (user_id, full_name, phone_number, role) VALUES (?, ?, ?, ?)",
+                   (data['user_id'], data['full_name'], data['phone_number'], data['role']))
+    conn.commit()
 
     await message.answer(f"Registration complete!\n\n"
                          f"Full Name: {data['full_name']}\n"
@@ -85,6 +101,8 @@ async def process_role(message: Message, state: FSMContext):
                          f"Role: {data['role']}\n\n"
                          "You are now signed up!")
 
+    # Finish the registration process and reset the state
+    await state.clear()
 
 async def main():
     bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
@@ -92,7 +110,6 @@ async def main():
     dp.include_router(form_router)
 
     await dp.start_polling(bot)
-
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
